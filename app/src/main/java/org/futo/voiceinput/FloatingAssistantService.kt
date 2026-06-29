afe07756079acd49c5aa2d205875511083f59ed1
KobiAmos2 <144587199+kobiamos2@users.noreply.github.com>
2026-06-29 15:44:45 +0300

    Update FloatingAssistantService.kt

--- app/src/main/java/org/futo/voiceinput/FloatingAssistantService.kt
+++ app/src/main/java/org/futo/voiceinput/FloatingAssistantService.kt
@@ -1,3 +1,35 @@
+package org.futo.voiceinput
+
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.content.pm.ServiceInfo
+import android.graphics.Color
+import android.graphics.PixelFormat
+import android.graphics.drawable.GradientDrawable
+import android.os.Build
+import android.os.Handler
+import android.os.IBinder
+import android.os.Looper
+import android.os.PowerManager
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.MotionEvent
+import android.view.View
+import android.view.WindowManager
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.app.NotificationCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
+import androidx.lifecycle.lifecycleScope
+
 class FloatingAssistantService : Service(), LifecycleOwner {
 
     private val lifecycleRegistry = LifecycleRegistry(this)
